3d2f253807b2d7a9ef07aba47f2a65c9b1d0bb3d
M67E863 <ae09vf@insim.biz>
2025-06-09 18:57:37 +0200

    feat: add --verbose option

--- main.py
+++ main.py
@@ -2,22 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    # these are the arguments that a user typed
     script_args = sys.argv
 
     if len(script_args) < 2:
         print("not enough arguments provided to script")
         exit(1)
 
+
+
     client = genai.Client(api_key=api_key)
+    user_prompt = script_args[1]
+    genai.types.Content()
+
+    ## create a "user" role in the conversation
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     content = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=script_args[1]
+        contents=messages
     )
 
-    print("Prompt tokens:", content.usage_metadata.prompt_token_count)
-    print("Response tokens:", content.usage_metadata.candidates_token_count)
+    if len(script_args) == 3 and script_args[2] == "--verbose":
+        print("User prompt:", content.text)
+        print("Prompt tokens:", content.usage_metadata.prompt_token_count)
+        print("Response tokens:", content.usage_metadata.candidates_token_count)
