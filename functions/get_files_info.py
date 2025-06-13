af11486db998430690795e443742eb12449a7e44
M67E863 <ae09vf@insim.biz>
2025-06-13 09:57:10 +0200

    feat: add write file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -50,3 +50,29 @@ def get_file_content(working_directory, file_path):
         return f"Error: {e}"
 
     return file_content_string
+
+def write_file(working_directory, file_path, content):
+    file_to_check = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not file_to_check.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot write "{file_path}" as it is outside the permitted working directory'
+
+    # if the file does not exist, we create it
+    if not os.path.isfile(file_to_check):
+        try:
+            os.makedirs(os.path.dirname(file_to_check), exist_ok=True)
+        except Exception as e:
+            return f"Error: {e}"
+
+    try:
+        ## write content on file
+        with open(file_to_check, "w") as f:
+            f.write(content)
+    except Exception as e:
+            return f"Error: {e}"
+
+
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+
--- tests.py
+++ tests.py
@@ -1,22 +1,22 @@
 import unittest
-from functions.get_files_info import get_file_content
+from functions.get_files_info import write_file
 
 
 class TestGetFilesInfo(unittest.TestCase):
     def test_calculator_files(self):
-        result = get_file_content("calculator", "main.py")
+        result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
         print(result)
-        # self.assertEqual(result, expected)
 
     def test_calculator_pkg_files(self):
-        result = get_file_content("calculator", "pkg/calculator.py")
+        result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
         print(result)
-        # self.assertEqual(result, expected)
 
+    ## a path that starts with / like /tmp/temp.txt is outside the working directory
+    ## In many operating systems, a path that starts with / indicates an absolute path, meaning it's relative to the root of the file system
     def test_calculator_bin(self):
-        result = get_file_content("calculator", "/bin/cat")
+        result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
         print(result)
-        self.assertEqual(result, 'Error: File not found or is not a regular file: "/bin/cat"')
+        self.assertEqual(result, 'Error: Cannot write "/tmp/temp.txt" as it is outside the permitted working directory')
 
 if __name__ == "__main__":
     unittest.main()
