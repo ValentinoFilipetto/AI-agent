115ab4e7218a340db14316defb627ad059df6fad
M67E863 <ae09vf@insim.biz>
2025-06-13 09:12:00 +0200

    feat: add get files info

--- tests.py
+++ tests.py
@@ -1,37 +1,22 @@
 import unittest
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
+
 
 class TestGetFilesInfo(unittest.TestCase):
     def test_calculator_files(self):
-        expected = (
-            "tests.py: file_size=1342, is_dir=False\n"
-            "main.py: file_size=564, is_dir=False\n"
-            "pkg: file_size=4096, is_dir=True\n"
-        )
-        result = get_files_info("calculator", ".")
+        result = get_file_content("calculator", "main.py")
         print(result)
-        self.assertEqual(result, expected)
+        # self.assertEqual(result, expected)
 
     def test_calculator_pkg_files(self):
-        expected = (
-            "render.py: file_size=753, is_dir=False\n"
-            "calculator.py: file_size=1720, is_dir=False\n"
-            "__pycache__: file_size=4096, is_dir=True\n"
-        )
-        result = get_files_info("calculator", "pkg")
+        result = get_file_content("calculator", "pkg/calculator.py")
         print(result)
-        self.assertEqual(result, expected)
+        # self.assertEqual(result, expected)
 
     def test_calculator_bin(self):
-        result = get_files_info("calculator", "/bin")
+        result = get_file_content("calculator", "/bin/cat")
         print(result)
-        self.assertEqual(result, 'Error: Cannot list "/bin" as it is outside the permitted working directory')
-
-    def test_calculator_baxk(self):
-        result = get_files_info("calculator", "../")
-        print(result)
-        self.assertEqual(result, 'Error: Cannot list "../" as it is outside the permitted working directory')
-
+        self.assertEqual(result, 'Error: File not found or is not a regular file: "/bin/cat"')
 
 if __name__ == "__main__":
     unittest.main()
