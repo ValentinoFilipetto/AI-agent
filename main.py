9732023f8deaf96913bf637f97cd7466766accc3
M67E863 <ae09vf@insim.biz>
2025-06-14 20:17:54 +0200

    feat: add all schemas

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from function_schemas import (
+    schema_get_file_content,
+    schema_get_files_info,
+    schema_run_python_file,
+    schema_write_file,
+)
+
 
 def main():
     load_dotenv()
@@ -12,6 +19,9 @@ def main():
                     You are a helpful AI coding agent.
                     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
                     - List files and directories
+                    - Read file contents
+                    - Execute Python files with optional arguments
+                    - Write or overwrite files
                     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
                     """
 
@@ -30,23 +40,12 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    schema_get_files_info = types.FunctionDeclaration(
-        name="get_files_info",
-        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "directory": types.Schema(
-                    type=types.Type.STRING,
-                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-                ),
-            },
-        ),
-    )
-
     available_functions = types.Tool(
         function_declarations=[
             schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
         ]
     )
 
